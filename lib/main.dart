10bed3702d100c9c387a2a604baed2cbf6382c0b
yair-naor1 <yair.naor@intel.com>
2026-08-13 10:45:04 +0300

    Verify Flutter app runs on Android device

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter is awesome and I\'m so excited to create an app!'),
     );
   }
 }
